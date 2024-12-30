a95db68159c9d93ae0bfeb8809a09b1834a04079
muskangoyal0606 <149530915+muskangoyal0606@users.noreply.github.com>
2024-12-30 19:17:53 +0530

    Time: 51 ms (6.98%), Space: 45.1 MB (6.15%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,16 +1,36 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder result = new StringBuilder(s);
-
-        // Continue until no occurrence of `part` exists
-         while (result.toString().contains(part))  {
-            // Find the index of the first occurrence of `part`
-            int index = result.indexOf(part);
-
-            // Remove `part` from `result`
-            result.delete(index, index + part.length());
+       Stack<Character> stack = new Stack<>();
+    int L = part.length();
+    
+    // Iterate through the string s
+    for (int i = 0; i < s.length(); i++) {
+        // Push the current character onto the stack
+        stack.push(s.charAt(i));
+        
+        // Check if the stack has enough characters to form 'part'
+        if (stack.size() >= L) {
+            // Create a temporary string from the top of the stack
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < L; j++) {
+                sb.append(stack.get(stack.size() - L + j));
+            }
+            
+            // If the top of the stack forms the substring 'part', pop those characters
+            if (sb.toString().equals(part)) {
+                for (int j = 0; j < L; j++) {
+                    stack.pop();
+                }
+            }
         }
-
-        return result.toString();
+    }
+    
+    // Convert the stack back into a string
+    StringBuilder result = new StringBuilder();
+    for (Character c : stack) {
+        result.append(c);
+    }
+    
+    return result.toString();
     }
 }
