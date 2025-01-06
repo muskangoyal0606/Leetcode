3134909b20b61c1fca5334b24dfa4419d63a5415
muskangoyal0606 <149530915+muskangoyal0606@users.noreply.github.com>
2025-01-06 18:32:37 +0530

    Time: 0 ms (100%), Space: 50 MB (84.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,35 +1,29 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            // Check if mid is at the correct position
-            boolean isEvenIndex = (mid % 2 == 0);
-            
-            if (nums[mid] == nums[mid + 1]) {
-                // Pair is on the right side
-                if (isEvenIndex) {
-                    left = mid + 2;
-                } else {
-                    right = mid - 1;
-                }
-            } else if (nums[mid] == nums[mid - 1]) {
-                // Pair is on the left side
-                if (isEvenIndex) {
-                    right = mid - 2;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                // Found the single element
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        int low=1;
+        int high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
+            else if((mid%2==1 && nums[mid]==nums[mid-1])||(mid%2==0 && nums[mid]==nums[mid+1])){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        
-        // Left points to the single element
-        return nums[left];
+        return -1;
     }
 }
