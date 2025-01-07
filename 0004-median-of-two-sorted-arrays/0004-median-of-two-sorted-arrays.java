453f70cd2d79b341f1ace6a6fb36ae80b6c7ac28
muskangoyal0606 <149530915+muskangoyal0606@users.noreply.github.com>
2025-01-07 20:06:40 +0530

    Time: 1 ms (100%), Space: 46 MB (63.12%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,54 +1,42 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       int n1 = nums1.length;  // Length of the first array
-        int n2 = nums2.length;  // Length of the second array
-        int n = n1 + n2;        // Total length of the combined array
-        
-        int ind1 = (n - 1) / 2;  // First median index
-        int ind2 = n / 2;        // Second median index (same as ind1 for odd lengths)
-        
-        int cnt = 0;   // Count of merged elements so far
-        int i = 0, j = 0;  // Pointers for nums1 and nums2
-        int ind1el = -1, ind2el = -1;  // Median elements
+int n1 = nums1.length, n2 = nums2.length;
 
-        // Traverse nums1 and nums2 simultaneously
-        while (i < n1 && j < n2) {
-            int val;
-            if (nums1[i] <= nums2[j]) {
-                val = nums1[i++];
+    // Ensure the first array is smaller or equal in size
+    if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+    int n = n1 + n2; // Total length of combined arrays
+    int left = (n1 + n2 + 1) / 2; // Elements in the left half of the partition
+
+    int low = 0, high = n1;
+    while (low <= high) {
+        int mid1 = (low + high) / 2;
+        int mid2 = left - mid1;
+
+        // Define partition values
+        int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+        int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+        int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+        int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+        // Check if the partitions are correct
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) {
+                // Odd total length: median is the max of the left side
+                return Math.max(l1, l2);
             } else {
-                val = nums2[j++];
+                // Even total length: median is the average of two middle values
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
             }
-            
-            if (cnt == ind1) ind1el = val;
-            if (cnt == ind2) ind2el = val;
-            cnt++;
-        }
-
-        // If nums1 has remaining elements
-        while (i < n1) {
-            if (cnt == ind1) ind1el = nums1[i];
-            if (cnt == ind2) ind2el = nums1[i];
-            i++;
-            cnt++;
-        }
-
-        // If nums2 has remaining elements
-        while (j < n2) {
-            if (cnt == ind1) ind1el = nums2[j];
-            if (cnt == ind2) ind2el = nums2[j];
-            j++;
-            cnt++;
-        }
-
-        // Calculate the median
-        if (n % 2 == 1) {
-            // Odd length: median is the middle element
-            return ind1el;
+        } else if (l1 > r2) {
+            // Move partition left in nums1
+            high = mid1 - 1;
         } else {
-            // Even length: median is the average of the two middle elements
-            return (ind1el + ind2el) / 2.0;
+            // Move partition right in nums1
+            low = mid1 + 1;
         }
+    }
+    return 0.0; // Should never reach here
     }
     
     
