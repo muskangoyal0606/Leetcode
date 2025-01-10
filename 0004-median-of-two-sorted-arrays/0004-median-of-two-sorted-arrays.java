5f7ea1079fb4f63c8d88345aef22ac98e2d60a1a
muskangoyal0606 <149530915+muskangoyal0606@users.noreply.github.com>
2025-01-10 09:41:47 +0530

    Time: 1 ms (100%), Space: 46.1 MB (51.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,42 +1,69 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-int n1 = nums1.length, n2 = nums2.length;
+    int n1 = nums1.length, n2 = nums2.length;
 
-    // Ensure the first array is smaller or equal in size
-    if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+    // Ensure the first array is the smaller one.
+    if (n1 > n2) {
+        int[] temp = nums1;
+        nums1 = nums2;
+        nums2 = temp;
+        n1 = nums1.length;
+        n2 = nums2.length;
+    }
 
     int n = n1 + n2; // Total length of combined arrays
-    int left = (n1 + n2 + 1) / 2; // Elements in the left half of the partition
+    int left = (n + 1) / 2; // Size of the left partition
 
     int low = 0, high = n1;
+
+    // Binary search to find the correct partition.
     while (low <= high) {
         int mid1 = (low + high) / 2;
         int mid2 = left - mid1;
 
-        // Define partition values
-        int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-        int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-        int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
-        int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+        // Values on either side of the partition.
+        int l1;
+        if (mid1 > 0) {
+            l1 = nums1[mid1 - 1];
+        } else {
+            l1 = Integer.MIN_VALUE;
+        }
 
-        // Check if the partitions are correct
+        int l2;
+        if (mid2 > 0) {
+            l2 = nums2[mid2 - 1];
+        } else {
+            l2 = Integer.MIN_VALUE;
+        }
+
+        int r1;
+        if (mid1 < n1) {
+            r1 = nums1[mid1];
+        } else {
+            r1 = Integer.MAX_VALUE;
+        }
+
+        int r2;
+        if (mid2 < n2) {
+            r2 = nums2[mid2];
+        } else {
+            r2 = Integer.MAX_VALUE;
+        }
+
         if (l1 <= r2 && l2 <= r1) {
             if (n % 2 == 1) {
-                // Odd total length: median is the max of the left side
                 return Math.max(l1, l2);
             } else {
-                // Even total length: median is the average of two middle values
-                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
             }
         } else if (l1 > r2) {
-            // Move partition left in nums1
             high = mid1 - 1;
         } else {
-            // Move partition right in nums1
             low = mid1 + 1;
         }
     }
-    return 0.0; // Should never reach here
+
+    return 0.0;
     }
     
     
