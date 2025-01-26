302644d169d6c02c746a0f9ce2f9dd2586671f1c
Muskan goyal <149530915+muskangoyal0606@users.noreply.github.com>
2025-01-26 14:51:33 +0530

    Time: 215 ms (54.09%), Space: 41.1 MB (70.76%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,58 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    // Try placing digits 1-9
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isSafe(char[][] board, int row, int col, char num) {
+        // Check row
+        for (int x = 0; x < 9; x++) {
+            if (board[row][x] == num) {
+                return false;
+            }
+        }
+        
+        // Check column
+        for (int x = 0; x < 9; x++) {
+            if (board[x][col] == num) {
+                return false;
+            }
+        }
+        
+        // Check 3x3 box using row/3 * 3 and col/3 * 3
+        int startRow = (row/3) * 3;
+        int startCol = (col/3) * 3;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
