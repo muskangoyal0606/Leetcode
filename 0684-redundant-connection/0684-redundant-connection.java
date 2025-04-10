6ecabac4561482db3e02e4b7ed9e28909d9635eb
Muskan goyal <149530915+muskangoyal0606@users.noreply.github.com>
2025-04-10 10:43:48 +0530

    Time: 18 ms (5.35%), Space: 45.6 MB (6.2%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int iscycle(int src, boolean[] visited, Map<Integer, List<Integer>> adj) {
+        Queue<Integer> q = new LinkedList<>();
+        Map<Integer, Integer> parent = new HashMap<>();
+        q.offer(src);
+        visited[src] = true;
+        parent.put(src, -1);
+        while (!q.isEmpty()) {
+            int front = q.poll();
+            for (int n : adj.getOrDefault(front, new ArrayList<>())) {
+                if (visited[n] && parent.get(front) != n) {
+                    return n;
+                } else if (!visited[n]) {
+                    visited[n] = true;
+                    q.offer(n);
+                    parent.put(n, front);
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int V = edges.length;
+        for (int i = 1; i <= V; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+
+        boolean[] visited = new boolean[V + 1];
+        int[] result = new int[2];
+
+        for (int i = 0; i < V; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
+            // Check cycle
+            Arrays.fill(visited, false);
+            if (iscycle(u, visited, adj) != -1) {
+                result = edges[i];
+                return result;
+            }
+        }
+
+        return null;
+    }
+}
