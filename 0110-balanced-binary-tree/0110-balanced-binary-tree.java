003eed07aa2e8e3165b7966220078ce54eab63ed
Muskan goyal <149530915+muskangoyal0606@users.noreply.github.com>
2025-08-08 11:56:32 +0530

    Time: 0 ms (100%), Space: 44.5 MB (56.91%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,21 +14,25 @@
  * }
  */
 class Solution {
+    private boolean isBalanced = true;
     public boolean isBalanced(TreeNode root) {
-        return height(root) != -1;
+        
+        check(root);
+        return isBalanced;
+    }
+    public int check(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int lh = check(root.left);
+        int rh = check(root.right);
+
+        if(Math.abs(lh-rh)>1){
+            isBalanced = false;
+        }
+
+        return Math.max(lh,rh)+1;
     }
 
-    private int height(TreeNode root) {
-        if (root == null) return 0; // Base case: empty tree has height 0
-
-        int lh = height(root.left);
-        if (lh == -1) return -1; // Left subtree is unbalanced
-
-        int rh = height(root.right);
-        if (rh == -1) return -1; // Right subtree is unbalanced
-
-        if (Math.abs(lh - rh) > 1) return -1; // Current tree is unbalanced
-
-        return Math.max(lh, rh) + 1; // Return correct height
-    }
+    
 }
