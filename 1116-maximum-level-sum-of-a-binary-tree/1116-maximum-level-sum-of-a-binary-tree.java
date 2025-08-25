bc73141db106b285e06bf6908f8bdbf22cba62e5
Muskan goyal <149530915+muskangoyal0606@users.noreply.github.com>
2025-08-25 21:36:27 +0530

    Time: 12 ms (9.39%), Space: 48 MB (8.64%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
@@ -1,42 +1,53 @@
-import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left, right;
-    TreeNode(int val) { this.val = val; }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        if (root == null) return 0;
-
-        int maxLevel = 1;
-        int level = 1;
-        int maxSum = Integer.MIN_VALUE;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int levelSum = 0;
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                levelSum += node.val;
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+        List<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> store = new ArrayList<>();
+        helper(root,0,result);
+        int sum = 0;
+        int max = 0;
+        for(List<Integer> a: result){
+            for(int n : a){
+                sum = sum +n;
+                
             }
-
-            if (levelSum > maxSum) {
-                maxSum = levelSum;
-                maxLevel = level;
-            }
-
-            level++;
+            store.add(sum);
+            sum =0;
         }
-
-        return maxLevel;
+        int maxSum = store.get(0);
+        int index = 0;
+        for (int i = 1; i < store.size(); i++) {
+            if (store.get(i) > maxSum) {
+                maxSum = store.get(i);
+                index = i;
+            }
+        }
+        return index +1;
+        
+    }
+    public void helper(TreeNode root, int level, List<List<Integer>> result){
+        if(root==null){
+            return;
+        }
+        if(result.size()==level){
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        helper(root.left,level+1,result);
+        helper(root.right,level+1,result);
     }
 }
